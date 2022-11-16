e5c737598e4959c80fdd25834c6789e846098bcf
utf-4096 <utf16+github@riseup.net>
2022-11-16 21:45:25 +0100

    Disable raw input mode

--- src/window.c
+++ src/window.c
@@ -231,8 +231,9 @@ void window_init() {
 	glfwSetScrollCallback(hud_window->impl, window_impl_mousescroll);
 	glfwSetCharCallback(hud_window->impl, window_impl_textinput);
 
-	if(glfwRawMouseMotionSupported())
-		glfwSetInputMode(hud_window->impl, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+	// This causes weird jumping issues, not sure why
+	// if(glfwRawMouseMotionSupported())
+	// 	glfwSetInputMode(hud_window->impl, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
 }
 
 void window_fromsettings() {
